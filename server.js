3c2024406f90b37868dbef1650f2f916cfa2b2ff
Audreycious <porcupine4@gmail.com>
2019-06-13 20:14:31 -0500

    genre and country working

--- server.js
+++ server.js
@@ -9,16 +9,32 @@ app.use(morgan('dev'))
 function movieFilter(req, res) {
     let response = movieListSmall;
     if (req.query.name) {
+        console.log('name ran')
         response = response.filter(movie => 
             movie.film_title.toLowerCase().includes(req.query.name.toLowerCase())
             )
     }
-    if (req.query.type) {
-        response = response.filter(movie => 
-            movie.film_title.toLowerCase().includes(req.query.name.toLowerCase())
-            )
+    else {
+        console.log('type ran')
+        let responseArr = [];
+        response.forEach(movie => {
+            // genre
+            if (movie.genre.toLowerCase() === req.query.type.toLowerCase()) {
+                responseArr.push(movie)
+            }
+            // country
+            else if (movie.country.toLowerCase() === req.query.type.toLowerCase()) {
+                responseArr.push(movie)
+            }
+            //  (req.query.type ===  ) {s
+            //     responseArr.push(movie)
+            // }
+            // avg_vote
+        // })
         // When theres no name, it means to filter by type: Change the if to an else
         // Take the query.type and check if it .include() genre, country, and avg_vote
+        })
+        response = responseArr
     }
     res.json(response)
 }
