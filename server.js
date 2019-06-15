a04d01a0c8357716a2c867eef8b1c52e42c7ad45
Emanuel Rouse <emanuelrouse@users.noreply.github.com>
2019-06-15 13:28:03 -0400

    'helmet implementation'

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const helmet = require('helmet')
 const cors = require('cors')
+const helmet = require('helmet')
 const movieListSmall = require('./movies-data-small.json')
 const app = express()
 
+
 app.use(morgan('dev'))
 app.use(cors())
-app.use(helmet())
+app.use(helmet.hidePoweredBy())
 app.use(validateRequest)
 
 function validateRequest(req, res, next) { 
-  const apiToken = process.env.API_TOKEN 
+  const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
 
 if (!authToken || authToken.split(' ')[1] !== apiToken) { 
